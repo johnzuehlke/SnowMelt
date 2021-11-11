0b3b915ef47fbc2f43180680bf17293c203ea590
John Zuehlke <johnzuehlke@users.noreply.github.com>
2021-11-10 21:34:33 -0600

    Updates post completed build

--- Snow_Master.ino
+++ Snow_Master.ino
@@ -14,6 +14,8 @@
 #define UECHO_PIN 12
 #define CS_SD_CARD_PIN 53
 #define IridiumSerial Serial1
+#define DIAGNOSTICS false // Set to true to see diagnostics
+
 #define SCALE1 Serial2
 #define SCALE2 Serial3
 // Low = receive | High = transmit
@@ -23,24 +25,26 @@
 #define SCALE2_POWER 6
 #define MODE_SEND HIGH
 #define MODE_RECV LOW
-#define DIAGNOSTICS true // Set to true to see diagnostics
 
 IridiumSBD modem(IridiumSerial);
 OneWire oneWire(TEMP_PIN);
 DallasTemperature temp_sensor(&oneWire);
 
+const float BAD_VAL = -99.99;
+const int ALRM_TIME_INTERVAL_HR = 12;
+const long DEPTH_OFFSET = 0; //Initial depth with no snow
+
 struct LogData {
   char time[21]; //time of measurements
-  float weight1 = -99.99; //weight on scale1
-  float weight2 = -99.99; //weight on scale2
-  float tempC = -99.99; //temp in celcius
-  float snow_depth = -99.99; //snow depth
-  float voltage = -99.99; //battery voltage
+  float weight1 = BAD_VAL; //weight on scale1
+  float weight2 = BAD_VAL; //weight on scale2
+  float tempC = BAD_VAL; //temp in celcius
+  float snow_depth = BAD_VAL; //snow depth
+  float voltage = BAD_VAL; //battery voltage
 };
 
 LogData logData;
 StaticJsonDocument<128> json_doc;
-int alarm_inteveral_minutes = 5;
 
 void setup() {
 
@@ -50,91 +54,122 @@ void setup() {
 
   // Start the serial port connected to the satellite modem
   IridiumSerial.begin(19200);
+  // If we're powering the device by USB, tell the library to
+  // relax timing constraints waiting for the supercap to recharge.
+  //modem.setPowerProfile(IridiumSBD::USB_POWER_PROFILE);
+  // For "high current" (battery-powered) applications
+  modem.setPowerProfile(IridiumSBD::DEFAULT_POWER_PROFILE);
+  set_time_from_rock_block();
 
   SCALE1.begin(9600);
   SCALE1.setTimeout(5000);
   SCALE2.begin(9600);
   SCALE2.setTimeout(5000);
 
-  // If we're powering the device by USB, tell the library to
-  // relax timing constraints waiting for the supercap to recharge.
-  modem.setPowerProfile(IridiumSBD::USB_POWER_PROFILE);
-  // For "high current" (battery-powered) applications
-  //modem.setPowerProfile(IridiumSBD::DEFAULT_POWER_PROFILE);
+  pinMode(SCALE1_POWER, OUTPUT);
+  pinMode(SCALE2_POWER, OUTPUT);
+  digitalWrite(SCALE1_POWER, LOW);
+  digitalWrite(SCALE2_POWER, LOW);
+  pinMode(SCALE1_MODE, OUTPUT);
+  pinMode(SCALE2_MODE, OUTPUT);
 
   pinMode(VOLTAGE_SENSOR_PIN, INPUT);
   pinMode(UTRIG_PIN, OUTPUT);
   pinMode(UECHO_PIN, INPUT);
 
-  //  set_time_from_rock_block();
+  // Use this block of code to set time on RTC module
+  //tmElements_t tm;
+  //tm.Hour = 21;
+  //tm.Minute = 12;
+  //tm.Second = 30;
+  //tm.Day = 10;
+  //tm.Month = 11;
+  //tm.Year = 2021 - 1970;
+  //RTC.write(tm);
+
   // initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
   setSyncProvider(RTC.get);   // the function to get the time from the RTC
   if (timeStatus() != timeSet)
     Serial.println("Unable to sync with the RTC");
   else
     Serial.println("RTC has set the system time");
-  //RTC.setAlarm(alarmType, seconds, minutes, hours, dayOrDate);
 
-  RTC.setAlarm(ALM1_MATCH_SECONDS, 5, 0, 0, 0);
+  // initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
+  RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
+  RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
   RTC.alarm(ALARM_1);
+  RTC.alarm(ALARM_2);
   RTC.alarmInterrupt(ALARM_1, false);
+  RTC.alarmInterrupt(ALARM_2, false);
   RTC.squareWave(SQWAVE_NONE);
+
+  //RTC.setAlarm(alarmType, seconds, minutes, hours, dayOrDate);
+  RTC.setAlarm(ALM1_MATCH_HOURS, 0, 0, 3, 0);
+  RTC.alarm(ALARM_1);
+
+  getSaveAndSendData();
 }
 
 void loop()
 {
-  if ( RTC.alarm(ALARM_1) )    // check alarm flag, clear it if set
-  {
+  if ( RTC.alarm(ALARM_1) )  {
+    Serial.println("Getting measurements...");
     time_t t = RTC.get();
-    int time_size = sprintf(logData.time, "%d-%02d-%02dT%02d:%02d:%02dZ", year(t), month(t), day(t), hour(t), minute(t), second(t));
-    //logData.weight1 = getScale(1);
-    //logData.weight2 = calculateWeight(2, getScale(2));
-    logData.voltage = get_voltage();
-    Serial.print("Current battery voltage is: ");
-    Serial.println(logData.voltage);
-    logData.tempC = get_temp_celcius();
-    Serial.print("Current temp is: ");
-    Serial.println(logData.tempC);
-    //logData.snow_depth = config.ultra_offset - read_distance();
-    logData.snow_depth = get_snow_depth(logData.tempC);
-    Serial.print("Current depth is: ");
-    Serial.println(logData.snow_depth);
-
-    create_json_from_data();
-    write_data_to_log_file();
-    send_data_to_rock_block();
-    json_doc.clear();
-
-    alarm_inteveral_minutes = (alarm_inteveral_minutes + 5) % 60;
-    RTC.setAlarm(ALM1_MATCH_MINUTES, 0, alarm_inteveral_minutes, 0, 0);
+    int new_alarm_time = (hour(t) + ALRM_TIME_INTERVAL_HR) % 24;
+    RTC.setAlarm(ALM1_MATCH_HOURS, 0, 0, new_alarm_time, 0);
     RTC.alarm(ALARM_1);
+
+    getSaveAndSendData();
   }
-  digitalClockDisplay();
+  //digitalClockDisplay();
   delay(1000);
 }
 
-long getScale(int scale) {
-  long value = 0;
+void getSaveAndSendData() {
+  Serial.println("Getting measurements...");
+  json_doc.clear();
+  time_t t = RTC.get();
+  int time_size = sprintf(logData.time, "%d-%02d-%02dT%02d:%02d:%02dZ", year(t), month(t), day(t), hour(t), minute(t), second(t));
+  logData.weight1 = getScale(1);
+  logData.weight2 = getScale(2);
+  logData.voltage = get_voltage();
+  Serial.print("Current battery voltage is: ");
+  Serial.println(logData.voltage);
+  logData.tempC = get_temp_celcius();
+  Serial.print("Current temp is: ");
+  Serial.println(logData.tempC);
+  logData.snow_depth = DEPTH_OFFSET - get_snow_depth(logData.tempC);
+  Serial.print("Current depth is: ");
+  Serial.println(logData.snow_depth);
+
+  create_json_from_data();
+  write_data_to_log_file();
+  send_data_to_rock_block();
+  json_doc.clear();
+}
+
+float getScale(int scale) {
+  float value = BAD_VAL;
   setSerialMode(MODE_RECV);
 
   switch (scale) {
     case 1:
       digitalWrite(SCALE1_POWER, HIGH);
-      value = readData(SCALE1);
+      value = readScaleData(SCALE1);
       digitalWrite(SCALE1_POWER, LOW);
       break;
     case 2:
       digitalWrite(SCALE2_POWER, HIGH);
-      value = readData(SCALE2);
+      value = readScaleData(SCALE2);
       digitalWrite(SCALE2_POWER, LOW);
       break;
   }
   return value;
 }
 
-float readData(Stream & port) {
+float readScaleData(Stream & port) {
   // Allows 5 errors because it will occasionally throw an Empty Input Error
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 15; i++) {
     setSerialMode(MODE_RECV);
 
     StaticJsonDocument<64> doc;
@@ -145,8 +180,7 @@ float readData(Stream & port) {
 
       if (err == DeserializationError::Ok) {
         const char* status = doc["status"];
-        float value = doc["value"];
-        return value;
+        return doc["value"];
       } else if (err == DeserializationError::EmptyInput) {
         // This error is expected b/c deserialze doesn't read the last null byte
         Serial.println("Empty input");
@@ -154,39 +188,56 @@ float readData(Stream & port) {
         Serial.println("Deserialization error");
       }
     } else {
-      Serial.println("Scale data not available");
+      Serial.println("Scale data not available. Checking again...");
       delay(1000);
     }
   }
-  return -99.99;
+  return BAD_VAL;
 }
 
 void setSerialMode(int mode) {
   digitalWrite(SCALE1_MODE, mode);
   digitalWrite(SCALE2_MODE, mode);
-  delay(1);
+  delay(50);
 }
 
 float get_voltage() {
   float R1 = 30000.0;
   float R2 = 7500.0;
-  float vout = (analogRead(VOLTAGE_SENSOR_PIN) * 5.0) / 1024.0;
-  return (vout / (R2 / (R1 + R2)));
+  uint8_t times = 5;
+  float s[times];
+  for (uint8_t i = 0; i < times; i++) {
+    float vout = (analogRead(VOLTAGE_SENSOR_PIN) * 5.0) / 1024.0;
+    s[i] = (vout / (R2 / (R1 + R2)));
+  }
+  insertSort(s, times);
+  if (times & 0x01) return s[times / 2];
+  return (s[times / 2] + s[times / 2 + 1]) / 2;
 }
 
 float get_temp_celcius() {
-  temp_sensor.requestTemperatures();
-  return (temp_sensor.getTempFByIndex(0) - 32) * 5 / 9;
+  uint8_t times = 5;
+  float s[times];
+  for (uint8_t i = 0; i < times; i++) {
+    temp_sensor.requestTemperatures();
+    s[i] = (temp_sensor.getTempFByIndex(0) - 32) * 5 / 9;
+    yield();
+    delay(50);
+  }
+  insertSort(s, times);
+  if (times & 0x01) return s[times / 2];
+  return (s[times / 2] + s[times / 2 + 1]) / 2;
 }
 
 float get_snow_depth(float currTemp) {
 
   unsigned long maxDistanceDurationMicroSec;
   int maxDistanceCm = 900;
-  float return_distance_cm = -1;
-  int attempt = 0;
-  while (attempt < 10 && return_distance_cm == -1) {
-
+  uint8_t times = 15;
+  float s[times];
+  float speedOfSoundInCmPerMicroSec = 0.03313 + 0.0000606 * currTemp; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+  for (uint8_t i = 0; i < times; i++)
+  {
     // Make sure that trigger pin is LOW.
     digitalWrite(UTRIG_PIN, LOW);
     delayMicroseconds(2);
@@ -194,21 +245,36 @@ float get_snow_depth(float currTemp) {
     digitalWrite(UTRIG_PIN, HIGH);
     delayMicroseconds(10);
     digitalWrite(UTRIG_PIN, LOW);
-    float speedOfSoundInCmPerMicroSec = 0.03313 + 0.0000606 * currTemp; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
 
     // Compute max delay based on max distance with 25% margin in microseconds
     maxDistanceDurationMicroSec = 2.5 * maxDistanceCm / speedOfSoundInCmPerMicroSec;
 
     // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
     unsigned long durationMicroSec = pulseIn(UECHO_PIN, HIGH, maxDistanceDurationMicroSec); // can't measure beyond max distance
-
-    return_distance_cm = durationMicroSec / 2.0 * speedOfSoundInCmPerMicroSec;
-    if (return_distance_cm == 0 || return_distance_cm > maxDistanceCm) {
-      return_distance_cm = -1.0 ;
-    }
-    attempt++;
+    s[i] = durationMicroSec / 2.0 * speedOfSoundInCmPerMicroSec;
+    yield();
+    delay(100);
   }
-  return return_distance_cm;
+  insertSort(s, times);
+  if (times & 0x01) return s[times / 2];
+  return (s[times / 2] + s[times / 2 + 1]) / 2;
+}
+
+void insertSort(float * array, uint8_t size) {
+  uint8_t t, z;
+  float temp;
+  for (t = 1; t < size; t++)
+  {
+    z = t;
+    temp = array[z];
+    while ( (z > 0) && (temp < array[z - 1] ))
+    {
+      array[z] = array[z - 1];
+      z--;
+    }
+    array[z] = temp;
+    yield();
+  }
 }
 
 void create_json_from_data() {
@@ -216,11 +282,19 @@ void create_json_from_data() {
   json_doc["ts"] = logData.time;
 
   char weight1_buf[8];
-  dtostrf(logData.weight1, 4, 3, weight1_buf);
+  if (logData.weight1 == BAD_VAL) {
+    strcpy(weight1_buf, "NM");
+  } else {
+    dtostrf(logData.weight1, 4, 3, weight1_buf);
+  }
   json_doc["w1"] = weight1_buf;
 
   char weight2_buf[8];
-  dtostrf(logData.weight2, 4, 3, weight2_buf);
+  if (logData.weight2 == BAD_VAL) {
+    strcpy(weight2_buf, "NM");
+  } else {
+    dtostrf(logData.weight2, 4, 3, weight2_buf);
+  }
   json_doc["w2"] = weight2_buf;
 
   char temp_buf[8];
@@ -266,7 +340,6 @@ bool write_data_to_log_file() {
     // close the file:
     myLogDataFile.close();
     Serial.println("done.");
-
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening logData.log");
@@ -278,8 +351,7 @@ void set_time_from_rock_block() {
   // Begin satellite modem operation
   Serial.println("Starting modem...");
   int err = modem.begin();
-  if (err != ISBD_SUCCESS)
-  {
+  if (err != ISBD_SUCCESS) {
     Serial.print("Begin failed: error ");
     Serial.println(err);
     if (err == ISBD_NO_MODEM_DETECTED)
@@ -290,21 +362,17 @@ void set_time_from_rock_block() {
 
   struct tm t;
   err = modem.getSystemTime(t);
-  if (err == ISBD_SUCCESS)
-  {
+  if (err == ISBD_SUCCESS) {
     char buf[32];
     sprintf(buf, "%d-%02d-%02d %02d:%02d:%02d",
             t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
     Serial.print(F("Iridium time/date is "));
     Serial.println(buf);
     RTC.set(mktime(&t));
-  }
-  else if (err == ISBD_NO_NETWORK) // Did it fail because the transceiver has not yet seen the network?
-  {
+  } else if (err == ISBD_NO_NETWORK) {
+    // Did it fail because the transceiver has not yet seen the network?
     Serial.println(F("No network detected.  Waiting 10 seconds."));
-  }
-  else
-  {
+  } else {
     Serial.print(F("Unexpected error "));
     Serial.println(err);
   }
@@ -317,34 +385,39 @@ void send_data_to_rock_block() {
 
   // Begin satellite modem operation
   Serial.println("Starting modem...");
-  //  int err = modem.begin();
-  //  if (err != ISBD_SUCCESS)
-  //  {
-  //    Serial.print("Begin failed: error ");
-  //    Serial.println(err);
-  //    if (err == ISBD_NO_MODEM_DETECTED)
-  //      Serial.println("No modem detected: check wiring.");
-  //    modem.sleep();
-  //    return;
-  //  }
+  int err = modem.begin();
+  if (err != ISBD_SUCCESS) {
+    Serial.print("Begin failed: error ");
+    Serial.println(err);
+    if (err == ISBD_NO_MODEM_DETECTED)
+      Serial.println("No modem detected: check wiring.");
+    modem.sleep();
+    return;
+  }
 
   char buffer[128];
   serializeJson(json_doc, buffer);
-  size_t bufferSize = strlen(buffer);
-  Serial.print("Size of buffer -> ");
-  Serial.println(bufferSize);
-
   Serial.println(buffer);
+
+  err = modem.sendSBDText(buffer);
+  if (err != ISBD_SUCCESS) {
+    Serial.print("sendSBDBinary failed: error ");
+    Serial.println(err);
+    if (err == ISBD_SENDRECEIVE_TIMEOUT)
+      Serial.println("Try again with a better view of the sky.");
+  } else {
+    Serial.println("Data sent!");
+  }
+  modem.sleep();
+  return;
 }
 
 #if DIAGNOSTICS
-void ISBDConsoleCallback(IridiumSBD * device, char c)
-{
+void ISBDConsoleCallback(IridiumSBD * device, char c) {
   Serial.write(c);
 }
 
-void ISBDDiagsCallback(IridiumSBD * device, char c)
-{
+void ISBDDiagsCallback(IridiumSBD * device, char c) {
   Serial.write(c);
 }
 #endif
@@ -356,7 +429,7 @@ void digitalClockDisplay()
   printDigits(minute());
   printDigits(second());
   Serial.print(' ');
-  Serial.print(day());
+  Serial.print(month());
   Serial.print(' ');
   Serial.print(month());
   Serial.print(' ');
